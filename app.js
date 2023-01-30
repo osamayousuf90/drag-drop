3209d491d621abdc77905ff6d731a14cb61c5ec9
Osama Yousuf <94833503+osamayousuf90@users.noreply.github.com>
2023-01-30 16:04:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -43,3 +43,74 @@ export default function App() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// work with api
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://your-api.com/items")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  const onDragStart = (e, index) => {
+    e.dataTransfer.setData("index", index);
+  };
+
+  const onDrop = async (e, index) => {
+    const fromIndex = e.dataTransfer.getData("index");
+    const newItems = [...items];
+    const [removed] = newItems.splice(fromIndex, 1);
+    newItems.splice(index, 0, removed);
+    setItems(newItems);
+    await fetch("https://your-api.com/items", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ items: newItems }),
+    });
+  };
+
+  const onDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="App">
+      <h1>Real-Time Drag and Drop Array of Objects with API in React</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={item.id}>
+            <div
+              draggable
+              onDragStart={(e) => onDragStart(e, index)}
+              onDrop={(e) => onDrop(e, index)}
+              onDragOver={onDragOver}
+            >
+              {item.text}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
